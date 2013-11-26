8fb8db6f32b6cb311e65ccc6f3ebb3e00f6fac6f
yangbinqi <ybq@outlook.com>
2013-11-26 23:20:13 +0800

    scan image

--- OpenCVHello/OpenCVHello/OpenCVHello/OpenCVHello.cpp
+++ OpenCVHello/OpenCVHello/OpenCVHello/OpenCVHello.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 /*  For description look into the help() function. */
-#include "opencv2/core/core.hpp"
+#include <opencv2\core\core.hpp>
+#include <opencv2\highgui\highgui.hpp>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace cv;
@@ -21,6 +23,39 @@ void main()
 	v.push_back(3.01f);
 	cout << "Vector of floats via Mat =" << Mat(v) << endl << endl;
 
+	
+	Mat I,J;
+	int divideWith=10;
+	cout << "Start Read:" << endl;
+	I=cv::imread("C:\\Users\\ybq_000\\Desktop\\iphone5.jpg",IMREAD_COLOR);
+	uchar table[256];
+	for(int i=0;i<256;++i)
+	{
+		table[i]=divideWith*(i/divideWith);
+	}
+	cout << "End Read:" << endl;
+	cout << "Rows of I:" << I.channels()* I.rows << endl;
+	cout << "Cols of I:" << I.cols << endl;
+}
 
-
+//the first method ,C Style
+Mat& ScanImageReduceC(Mat& I,const uchar* const table)
+{
+	CV_Assert(I.depth()==sizeof(uchar));
+	int cols=I.channels()*I.rows;
+	int rows=I.rows;
+	if(I.isContinuous())
+	{
+		cols*=rows;
+		rows=1;
+	}
+	int i,j;
+	uchar* p;
+	for(i=0;i++;i<rows){
+		p=I.ptr(i);
+		for(j=0;j++;j<cols){
+			p[j]=table[p[j]];
+		}
+	}
+	return I;
 }
